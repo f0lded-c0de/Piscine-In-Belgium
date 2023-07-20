b20daa9509d439ea6a20b600f419d416a0b68fdf
Barth <barth.samzun@gmail.com>
2023-07-20 17:06:56 +0200

    Okay, I fixed the 80 columns lines thing in ex06 but HOLY SHIT was that annoying, I did NOT manage to do it without declaring a subfunction. Although I will say, that sub function might be pretty useful so I'm not mad I made it

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -1,23 +1,37 @@
 #include <unistd.h>
 
+int ft_any(char arr[], int test, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (arr[i] != test)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void ft_print_comb2(void)
 {
 	char left[2];
 	char right[2];
 
 	left[0] = 48;
-	while(left[0] <= 57) {
+	while (left[0] <= 57) {
 		left[1] = 48;
-		while(left[1] <= 57) {
+		while (left[1] <= 57) {
 			right[0] = 48;
-			while(right[0] <= 57) {
+			while (right[0] <= 57) {
 				right[1] = 48;
-				while(right[1] <= 57) {
-						write(1, left, 2);
-						write(1, " ", 1);
-						write(1, right, 2);
-						if(left[0] != 57 || left[1] != 57 || right[0] != 57 || right[1] != 57)
-							write(1, ", ", 2);
+				while (right[1] <= 57) {
+					write(1, left, 2);
+					write(1, " ", 1);
+					write(1, right, 2);
+					if (ft_any(left, 57, 2) == 0 || ft_any(right, 57, 2) == 0)
+						write(1, ", ", 2);
 					right[1]++;
 				}
 				right[0]++;
