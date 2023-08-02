a306f0b0e35eca96701faa0af431426dd17c3ee6
f0ld <barth.samzun@gmail.com>
2023-08-02 09:45:06 +0000

    Started c01, completed ex00

---
+++ c01/ex00/main.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+void ft_ft(int *a);
+
+int main(void)
+{
+	int a;
+
+	a = 22;
+	ft_putnbr(a);
+	ft_ft(&a);
+	write(1, "\n", 1);
+	ft_putnbr(a);
+	write(1, "\n", 1);
+
+	return (0);
+}
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb > 9 || nb < -9)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			ft_putnbr((nb / 10) * -1);
+			ft_putnbr((nb % 10) * -1);
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putchar((nb * -1) + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
