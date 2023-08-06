e20dd7a1aba4af10597ee77a5ccc7342f8c25c87
f0ld <barth.samzun@gmail.com>
2023-08-06 16:46:44 +0000

    finished ex08

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -1,6 +1,7 @@
 char	*ft_strupcase(char *str)
 {
 	int i;
+
 	i = 0;
 	while(str[i])
 	{
---
+++ c02/ex08/main.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+char	*ft_strlowcase(char *str);
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb > 9 || nb < -9)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			ft_putnbr((nb / 10) * -1);
+			ft_putnbr((nb % 10) * -1);
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putchar((nb * -1) + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+        {
+	        write(1, &str[i], 1);
+	        i++;
+	}
+}
+
+int main(void)
+{
+	char	str[27] = "AbcDEfgHIJklmnOPQrStuVWxyZ\n";
+
+	ft_putstr(str);
+	ft_strlowcase(str);
+	ft_putstr(str);
+	return (0);
+}
