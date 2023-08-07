34c7a554b6c3497456ee2406eebf9629449c085b
f0ld <barth.samzun@gmail.com>
2023-08-07 14:37:12 +0000

    finished ex03

---
+++ c03/ex03/main.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+char	*ft_strncat(char *dest, char *src, unsigned int nb);
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb > 9 || nb < -9)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			ft_putnbr((nb / 10) * -1);
+			ft_putnbr((nb % 10) * -1);
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putchar((nb * -1) + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+        {
+	        write(1, &str[i], 1);
+	        i++;
+	}
+}
+
+int main(void)
+{
+	char	src[] = "123456789123456789\n";
+	char	dest[] = "123";
+
+	ft_putstr(src);
+	ft_putstr(dest);
+	write(1, "\n", 1);
+	ft_strncat(dest, src, 10);
+	ft_putstr(dest);
+	return (0);
+}
