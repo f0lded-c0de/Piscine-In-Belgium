4de2fa5337260404c78fa6625ccdafb22df017a3
f0ld <barth.samzun@gmail.com>
2023-08-09 15:45:07 +0000

    small correction to ex03

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -7,8 +7,7 @@ int	ft_atoi(char *str)
 	i = 0;
 	sign = 0;
 	result = 0;
-	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || 
-			str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
 		i++;
 	while (str[i] == '-' || str[i] == '+')
 	{
--- c04/ex03/main.c
+++ c04/ex03/main.c
@@ -4,7 +4,7 @@ int	ft_atoi(char *str);
 int	main(void)
 {
 	int	n;
-	char	str[] = "  \f\n\r\t\v\r\n\v \f \t ---++-+-+++--1grtgfdzet";
+	char	str[] = "  \f\n\r\t\v\r\n\v \f \t ---++-+-+++--1871grtgfdzet";
 
 	n = ft_atoi(str);
 	ft_putnbr(n);
