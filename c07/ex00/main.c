707ab274b551c921566dfb2ca2047424bd891064
f0ld <barth.samzun@gmail.com>
2023-08-16 13:55:56 +0000

    began c07, of which I just finished ex00

---
+++ c07/ex00/main.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdlib.h>
+char	*ft_strdup(char *src);
+int	ft_strlen(char *str);
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb > 9 || nb < -9)
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			ft_putnbr((nb / 10) * -1);
+			ft_putnbr((nb % 10) * -1);
+		}
+		else
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putchar((nb * -1) + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+        {
+	        write(1, &str[i], 1);
+	        i++;
+	}
+}
+
+int main(void)
+{
+	char	*dest;
+
+	dest = ft_strdup("");
+	ft_putstr(dest);
+	ft_putnbr(ft_strlen(dest));
+	write(1, "\n", 1);
+	free(dest);
+	ft_putstr(dest);
+	ft_putnbr(ft_strlen(dest));
+	return (0);
+}
